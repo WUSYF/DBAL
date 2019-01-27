0403e41bcf6acf53e79875a5e9043c783f0eb84e
Daniel <6140@htl.rennweg.at>
2019-01-27 22:22:40 +0100

    Erstes GIT KOMMIT

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 27.01.19
+ * Time: 20:20
+ */
+require_once 'vendor/autoload.php';
+
+$config = new \Doctrine\DBAL\Configuration();
+$connectionParams = array
+(
+    'dbname' => 'WEBT',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+$sql = "SELECT * FROM mitarbeiter";
+$sqlReturn = $conn->query($sql);
+
+while ($row = $sqlReturn->fetch()){
+    echo $row['nachname'].' ';
+    echo $row['vorname'].' ';
+    echo $row['gebDatum'].' ';
+    echo $row['PLZ'].' ';
+    echo $row['strasse'].' ';
+    echo "<br><br>";
+}
